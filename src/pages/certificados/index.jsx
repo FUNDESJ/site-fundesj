26511417e78c77b6dde8e93208a94c7dd4c5bc40
FUNDESJ <tecnologia@fundesj.com.br>
2025-05-22 17:29:07 -0300

    Site-Fundesj

--- src/pages/certificados/index.jsx
+++ src/pages/certificados/index.jsx
@@ -1,9 +1,25 @@
 import './certificados.css'
+import axios from 'axios'
 import Footer from '../footer/footer';
 import Header from '../header/index.jsx';
 import Imagem1 from '../../images/certificado.png';
+import { useState } from 'react';
 
 function Certificados() {
+    const [codigo, setCodigo] = useState('')
+    const certificado = async () => {
+        if (!codigo) {
+            return alert("Informe o codigo do seu certificado")
+        }
+        try {
+            const resposta = await axios.get(`https://back-end-fundesj.onrender.com/certificado/${codigo}`,
+            )
+            console.log(resposta.data)
+        } catch (erro) {
+            alert("Certificado nao encontrado");
+            console.log("Certificado nao encontrado")
+        }
+    }
 
     return (
         <div className="certificados-container">
@@ -20,8 +36,10 @@ function Certificados() {
                         type="text"
                         placeholder="Digite o código de verificação"
                         className="verificador-input"
+                        value={codigo}
+                        onChange={((e) => setCodigo(e.target.value))}
                     />
-                    <button className="verificador-botao">Procurar</button>
+                    <button className="verificador-botao" onClick={certificado}>Procurar</button>
 
                 </div>
             </div>
