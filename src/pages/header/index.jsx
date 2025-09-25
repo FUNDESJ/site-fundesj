144e7889bfe2d4b907cbbeb9eccd48369adaee27
felinharess <felinharesdomingues@gmail.com>
2025-09-25 10:29:36 -0300

    correcao do header responsivo

--- src/pages/header/index.jsx
+++ src/pages/header/index.jsx
@@ -4,8 +4,17 @@ import { Link } from 'react-router-dom';
 import './header.css';
 
 export default function Header() {
-    
   const [openMenu, setOpenMenu] = useState(null);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  // Abre/fecha submenu no clique (mobile)
+  const handleSubmenuClick = (menuName) => {
+    if (openMenu === menuName) {
+      setOpenMenu(null);
+    } else {
+      setOpenMenu(menuName);
+    }
+  };
 
   return (
     <header className="navbar">
@@ -13,20 +22,31 @@ export default function Header() {
         <img src={logo} alt="Logo" className="logo" />
       </Link>
 
-      <nav className="menu">
+      {/* Botão hamburguer (aparece no mobile) */}
+      <div
+        className="menu-toggle"
+        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <nav className={`menu ${isMobileMenuOpen ? 'active' : ''}`}>
         <ul>
           {/* A INSTITUIÇÃO */}
           <li
             className="dropdown has-submenu"
-            onMouseEnter={() => setOpenMenu('instituicao')}
-            onMouseLeave={() => setOpenMenu(null)}
+            onMouseEnter={() => !isMobileMenuOpen && setOpenMenu('instituicao')}
+            onMouseLeave={() => !isMobileMenuOpen && setOpenMenu(null)}
+            onClick={() => isMobileMenuOpen && handleSubmenuClick('instituicao')}
           >
             <Link to="/instituição">
               A INSTITUIÇÃO
             </Link>
 
             {openMenu === 'instituicao' && (
-              <ul className="submenu">
+              <ul className="submenu open">
                 <li><Link to="/equipe" className="submenu-link">Equipe</Link></li>
                 <li><Link to="/realizações" className="submenu-link">Realizações 2024</Link></li>
                 <li><Link to="/parceiros" className="submenu-link">Parceiros</Link></li>
@@ -35,10 +55,7 @@ export default function Header() {
             )}
           </li>
 
-
-          
-
-          {/* Outros itens sem submenu */}
+          {/* Outros itens */}
           <li><Link to="/validador">CERTIFICADOS</Link></li>
           <li><Link to="/noticias">NOTÍCIAS</Link></li>
           <li><Link to="/usj">USJ</Link></li>
